da6654d3b24574d312cd57375fcf10073cceb87b
Tropic <tropical.productions.scratch@gmail.com>
2017-01-07 22:57:15 +0100

    Added invalid refund outcome

--- src/main/java/com/Acrobot/ChestShop/Events/PreShopCreationEvent.java
+++ src/main/java/com/Acrobot/ChestShop/Events/PreShopCreationEvent.java
@@ -143,6 +143,7 @@ public class PreShopCreationEvent extends Event {
         INVALID_QUANTITY,
 
         SELL_PRICE_HIGHER_THAN_BUY_PRICE,
+        REFUND_HIGHER_THAN_BUY_PRICE,
 
         NO_CHEST,
 
